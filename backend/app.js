8a4194c42690a571993fb30d6ccf4c33df7dddf1
Abdu Uralov <auralov@wesleyan.edu>
2025-07-19 17:36:19 -0400

    print text in app.js when get request

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,9 @@ app.listen(process.env.PORT || 3000, () =>
   console.log(`Server running on port ${process.env.PORT || 3000}`)
 );
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 //verbose logging middleware
 app.use((req, res, next) => {
